1589de3a076611b8edfb1c656980d4305a356854
Axel Bruneaux <gazhrot@gmail.com>
2017-01-23 10:12:12 +0100

    adding join room, send socket when user join room, and broadcast msg to room

--- app.js
+++ app.js
@@ -11,26 +11,25 @@ app.get('/', function (req, res) {
 
 io.sockets.on('connection', function (socket, pseudo) {
     // Dès qu'on nous donne un pseudo, on le stocke en variable de session et on informe les autres personnes
-    console.log(socket.id);
+    //console.log(socket.id);
     socket.on('nouveau_client', function(pseudo) {
         pseudo = ent.encode(pseudo);
         socket.pseudo = pseudo;
         socket.broadcast.emit('nouveau_client', pseudo);
     });
 
+    socket.on('room', function(room, pseudo) {
+        socket.emit('joined_room', pseudo, room);
+        socket.join(room);
+        console.log(room,pseudo);
+    });
+
     // Dès qu'on reçoit un message, on récupère le pseudo de son auteur et on le transmet aux autres personnes
-    socket.on('message', function (message) {
-        if (message === '/kick') {
-            socket.emit('kick');
-            socket.on('kick_client', function(client_id) {
-                console.log(client_id);
-                io.sockets.connected[client_id].emit('disconnect', {location: 'http://www.google.com', client_id: client_id});
-                io.sockets.connected[client_id].disconnect();
-            })
-        } else {
-            message = ent.encode(message);
-            socket.broadcast.emit('message', {pseudo: socket.pseudo, message: message});
-        }
+    socket.on('message', function (message, room) {
+        console.log(socket.rooms);
+        message = ent.encode(message);
+        io.sockets.in(room).emit('message', {pseudo: socket.pseudo, message: message, room: room});
+        //socket.broadcast.emit('message', {pseudo: socket.pseudo, message: message});
     }); 
 });
 
